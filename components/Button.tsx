b6f5fc342aaf5ee75ce19c57104f0f1cf06d265b
muzuvajoshua <muzuvajoshua@gmail.com>
2024-12-13 18:05:48 +0300

    l have done the setup, intial code including the navbar and the hero section.

---
+++ components/Button.tsx
@@ -0,0 +1,20 @@
+type ButtonProps = {
+  type: "submit" | "button";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green" | "btn_green" | "btn_white_text";
+};
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      type={type}
+    >
+      {icon && <img src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
+};
+
+export default Button;
